1fb17e63d98bc778ab1c4c7489fddd11fbed935f
karthik <jkarthik1947@gmail.com>
2024-05-22 00:21:24 +0530

    Update delete post functionality

--- src/app/components/Icons.jsx
+++ src/app/components/Icons.jsx
@@ -4,11 +4,13 @@ import { signIn, useSession } from "next-auth/react"
 import { collection, doc, getFirestore, onSnapshot, serverTimestamp, setDoc, snapshotEqual, deleteDoc} from "firebase/firestore";
 import { app } from "@/firebase"
 import { useEffect, useState } from "react";
-export default function Icons({ id }) {
+export default function Icons({ id, uid }) {
     const { data: session } = useSession();
     const [isLiked, setIsLiked] = useState(false)
     const [likes, setLikes] = useState([])
     const db = getFirestore(app)
+
+     
     const likePost = async () => {
         if(session){
             if(isLiked){
@@ -35,6 +37,22 @@ export default function Icons({ id }) {
     useEffect(() => {
         setIsLiked(likes.findIndex((like) => like.id === session?.user?.uid) !== -1)
     }, [likes])
+
+    const deletePost = async () => {
+        if(window.confirm('Are you sure you want to delete this post?')) {
+            if(session?.user?.uid === uid) {
+                deleteDoc(doc(db, 'posts', id)).then(() => {
+                    console.log("Document deleted")
+                    window.location.reload()
+                }).catch((error) => {
+                    console.log('Error removing the post', error)
+                })
+            }
+            else{
+                alert('You are not authorized to delete this post')
+            }
+        }
+    }
   return (
     <div className="flex justify-start gap-5 p-2 text-gray-500">
         <HiOutlineChat className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100"/>
@@ -51,8 +69,7 @@ export default function Icons({ id }) {
         {likes.length > 0 && <span className={`text-xs ${isLiked && "text-red-600"}`}>{likes.length}</span>}
         </div>
         
-        
-        <HiOutlineTrash className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-sky-100"/>
+        {session?.user?.uid === uid && (<HiOutlineTrash className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-sky-100" onClick={deletePost}/>)}
     </div>
   )
 }

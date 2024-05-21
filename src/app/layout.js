ac21489dba1f2b5483df3e4d7823ea0feb0b631b
karthik <jkarthik1947@gmail.com>
2024-05-21 16:40:51 +0530

    Completed News component

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,8 +21,13 @@ export default function RootLayout({ children }) {
         <div>
         {children}
         </div>
+        <div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
+        <div className="sticky top-0 bg-white py-2">
+          <input type="text" placeholder="Search" className="bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2"></input>
+        </div>
         <div>
           <News/>
+        </div>
         </div>
         </div>
         </body>
